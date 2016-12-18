907dfedad41631bed06a8ffb88efcd5a3bd09f22
DeligiannisGeorge <g.d.deligiannis@gmail.com>
2016-12-18 17:40:54 +0200

    add all

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/vanbartzi.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    	console.log('response', response.courses.completed);
+    	addCourses(response.courses.completed);
+    }
+  });
+
+
+  function addCourses(courses) {
+
+  	courses.forEach(function(course){
+  		var $course = $('<div />', {
+  			'class': 'course'
+  		}).appendTo( $("#badges"));
+
+  		$('<h3 />', {
+  			text: course.title
+  		}).appendTo($course);
+
+  		$('<img />', {
+  			src: course.badge
+  		}).appendTo($course);
+  		$('<a />', {
+  			'class': 'btn btn-primary',
+  			target: '_blank',
+  			href: course.url,
+  			text: 'See course'
+  		}).appendTo($course);
+  	})
+
+  }
 
 });
